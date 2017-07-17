600ce7a81acedb128df72d7237d7436ab7a5769f
swebb1 <swebb1@staffmail.ed.ac.uk>
2017-07-17 13:35:37 +0100

    Added colours and SD to rollPlot

--- R/rollPlot.r
+++ R/rollPlot.r
@@ -14,6 +14,9 @@
 #' @param feature Name of features being binned. Defaults to "data points".
 #' @param data Returns a table in long form if set to True.
 #' @param samples Set the legend name. Defaults to "Samples".
+#' @param split Plot samples separately.
+#' @param facet Variable to facet by.
+#' @param sd Plot standard deviation as ribbon when plotting mean.
 #' @keywords bin average plots.
 #' @export
 #' @examples
@@ -21,7 +24,7 @@
 
 
 rollPlot<-function(t,x,y,ylab="",xlab="",w=400,s=80,f="mean",
-                   scale="linear",feature="data points",data=F,samples="Samples",facet=F,sd=F){
+                   scale="linear",feature="data points",data=F,samples="Samples",split=F,sd=F,cols=vector(),facet=NA){
   
   library(zoo) #for rollapply function
   library(ggplot2)
@@ -44,7 +47,6 @@ rollPlot<-function(t,x,y,ylab="",xlab="",w=400,s=80,f="mean",
     ms$variable<-factor(ms$variable,levels=y)
     ms$idx<-as.numeric(as.character(ms$idx))
   }
-  
   if(data){
     if(sd & f=="mean"){
       return(list(a=m,sd=ms))
@@ -56,8 +58,13 @@ rollPlot<-function(t,x,y,ylab="",xlab="",w=400,s=80,f="mean",
   if(scale!="bins"){
     p<-ggplot(m,aes_string(x,"value",colour="variable"))
     if(sd & f=="mean"){
-      p<-p+geom_ribbon(aes(ymin = value - ms$value , ymax = value + ms$value, fill = variable),alpha=0.5,colour=NA)+
-      scale_fill_viridis(discrete=T,guide=F)
+      p<-p+geom_ribbon(aes(ymin = value - ms$value , ymax = value + ms$value, fill = variable),alpha=0.5,colour=NA)
+      if(length(cols)==0){
+        p<-p+scale_fill_viridis(discrete=T,guide=F)
+      }
+      else{
+        p<-p+scale_fill_manual(values = cols)
+      }
     }
     p<-p+geom_line(size=2)+ggtitle(paste(l,feature))+theme_bw()
     if(scale=="log"){
@@ -68,15 +75,30 @@ rollPlot<-function(t,x,y,ylab="",xlab="",w=400,s=80,f="mean",
     }
   }
   else{
-    p<-ggplot(m,aes(x=idx,y=value,colour=variable,group=variable))+geom_line(size=2)+ggtitle(paste(l,feature))+theme_bw()
+    p<-ggplot(m,aes(x=idx,y=value,colour=variable,group=variable))
+    if(sd & f=="mean"){
+      p<-p+geom_ribbon(aes(ymin = value - ms$value , ymax = value + ms$value, fill = variable),alpha=0.5,colour=NA)
+      if(length(cols)==0){
+        p<-p+scale_fill_viridis(discrete=T,guide=F)
+      }
+      else{
+        p<-p+scale_fill_manual(values = cols)
+      }
+    }
+    p<-p+geom_line(size=2)+ggtitle(paste(l,feature))+theme_bw()
   }
   if(ylab!=""){
     p<-p+ylab(ylab)
   }
-  p<-p+scale_color_viridis(discrete=T,name=samples)
-  if(facet){
+  if(length(cols)==0){
+    p<-p+scale_color_viridis(discrete=T,name=samples)
+  }
+  else{
+    p<-p+scale_color_manual(values = cols)
+  }
+  if(split){
     p<-p+facet_wrap(~variable)
   }
-  p
+p
 }
 
